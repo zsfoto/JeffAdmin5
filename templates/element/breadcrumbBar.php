980a76e1b1abc0c926bb907a392342cdacabac7d
Jeff Shoemaker <zsfoto@gmail.com>
2024-04-09 15:15:37 +0200

    Fixed index bake and fixed template bake

--- templates/element/breadcrumbBar.php
+++ templates/element/breadcrumbBar.php
@@ -48,13 +48,13 @@ if($controller == 'Users'){
 						<div class="breadcrumb-holder pt-1 pb-2">
 							<?php
 
-	if( $config['header_buttons_in_action'][$action]["back"]){
+	if(isset($config['header_buttons_in_action'][$action]["back"]) && $config['header_buttons_in_action'][$action]["back"]){
 		echo $this->Html->link('<span class="btn-label"><i class="fa fa-arrow-left"></i></span>' . __("Back to list"),
 			["action" => "index", "fullBase" => true, '?' => $queryParams, '#' => $lastId],
 			["escape" => false, "role" => "button", "class" => "btn btn-outline-secondary mt-1 me-1"]
 		);		
 	}
-	if( $config['header_buttons_in_action'][$action]["save"]){
+	if(isset($config['header_buttons_in_action'][$action]["save"]) && $config['header_buttons_in_action'][$action]["save"]){
 		echo $this->Html->link('<span class="btn-label"><i class="fa fa-save"></i></span>' . __d('jeff_admin', 'Save'), '#', 
 			[
 				'id' => 'button-submit', 'class'=>'btn btn-primary mt-1 me-1', 'role'=>'button', 'escape'=>false,  
@@ -62,19 +62,19 @@ if($controller == 'Users'){
 			]
 		);		
 	}
-	if( $config['header_buttons_in_action'][$action]["add"]){
+	if(isset($config['header_buttons_in_action'][$action]["add"]) && $config['header_buttons_in_action'][$action]["add"]){
 		echo $this->Html->link('<span class="btn-label"><i class="fa fa-plus"></i></span>' . __("Add new") . '&nbsp;',
 			["action" => "add", "fullBase" => true],
 			["escape" => false, "role" => "button", "class" => "btn btn-success mt-1 me-1"]
 		);
 	}
-	if( $config['header_buttons_in_action'][$action]["edit"]){
+	if(isset($config['header_buttons_in_action'][$action]["edit"]) && $config['header_buttons_in_action'][$action]["edit"]){
 		echo $this->Html->link('<span class="btn-label"><i class="fa fa-edit"></i></span>' . __("Edit") . '&nbsp;&nbsp;',
 			["action" => "edit", $id, "fullBase" => true],
 			["escape" => false, "role" => "button", "class" => "btn btn-primary mt-1 me-1"]
 		);
 	}
-	if( $config['header_buttons_in_action'][$action]["view"]){
+	if(isset($config['header_buttons_in_action'][$action]["view"]) && $config['header_buttons_in_action'][$action]["view"]){
 		echo $this->Html->link('<span class="btn-label"><i class="fa fa-eye"></i></span>' . __("View") . '&nbsp;',
 			["action" => "view", $id, "fullBase" => true],
 			["escape" => false, "role" => "button", "class" => "btn btn-warning mt-1 me-1"]
@@ -90,14 +90,14 @@ if($controller == 'Users'){
 	//}
 
 	// floar-end
-	if( $config['header_buttons_in_action'][$action]["delete"]){
+	if(isset($config['header_buttons_in_action'][$action]["delete"]) && $config['header_buttons_in_action'][$action]["delete"]){
 	//	echo $this->Html->link('<span class="btn-label"><i class="fa fa-times"></i></span>' . __("Delete") . '&nbsp;',
 	//		["action" => "delete", "fullBase" => true],
 	//		["escape" => false, "role" => "button", "class" => "btn btn-danger mt-1 me-1 float-end"]
 	//	);
 	}
 
-	if( $config['header_buttons_in_action'][$action]["delete"]){
+	if(isset($config['header_buttons_in_action'][$action]["delete"]) && $config['header_buttons_in_action'][$action]["delete"]){
 		if(isset($id) && isset($name)){
 			echo $this->Form->postLink('', ['action' => 'delete', $id, "fullBase" => true], ['class'=>'hide-postlink index-delete-button-class', "escape" => false]);
 			echo '<a href="javascript:;" class="btn btn-danger mt-1 me-1 float-end postlink-delete" data-bs-tooltip="tooltip" data-bs-placement="top" title="' . __("Delete this record!") . '" text="' . $name .'" subText="' . __("You will not be able to revert this!") . '" confirmButtonText="' . __("Yes, delete it!") . '" cancelButtonText="' . __("Cancel") . '"><span class="btn-label"><i class="fa fa-minus"></i></span>' . __("Delete") . '</a>';
