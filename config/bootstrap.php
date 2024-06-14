5a0fb2da6debcd2e533e1c4f0606a0f31cb875e9
Jeff Shoemaker <zsfoto@gmail.com>
2024-06-14 15:39:07 +0200

    Set Version: 1.0.24

--- config/bootstrap.php
+++ config/bootstrap.php
@@ -11,7 +11,7 @@
 //die('aaaaaaaa');
 use Cake\Core\Configure;
 
-Configure::write('JeffAdmin', ['version' => '1.0.18']);
+Configure::write('JeffAdmin', ['version' => '1.0.24']);
 
 try {
     Configure::load('sidebarmenu', 'default');
